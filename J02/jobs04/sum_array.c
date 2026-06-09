e4c32f76f2658b0890ccb95e7bd82dcce2731f9f
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-06-09 11:04:46 +0200

    rajout jobs 04

---
+++ J02/jobs04/sum_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int sum_array(int *arr,int n)
+{
+ 
+    int sum =0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=*(arr+i);
+    }
+    return sum;
+}
+
+int main()
+{
+    int arr[5]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int sum = sum_array(arr,n);
+    printf("Sum : %d\n",sum);
+    return 0;
+}
