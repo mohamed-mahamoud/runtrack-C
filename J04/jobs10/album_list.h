c5d9c887a1e7b74995b6fed95c1a3cf7f22453c5
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-06-12 13:05:14 +0200

    rajout du jobs 10

---
+++ J04/jobs10/album_list.h
@@ -0,0 +1,30 @@
+#ifndef ALBUM_LIST_H
+#define ALBUM_LIST_H
+#include "../jobs02/album.h"
+#include "../jobs03/create_album.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct album_item{
+    char *artist;
+    char *title;
+    int year;
+    struct album_item *next;
+} AlbumItem;
+
+AlbumItem *create_album_item(const char *artist, const char *title, int year);
+
+int count_albums(AlbumItem *list);
+
+AlbumItem *find_album(AlbumItem *list, const char *title);
+
+void reverse_list(AlbumItem **list);
+
+void clear_list(AlbumItem **list);
+
+void push_back_album(AlbumItem **list, AlbumItem *new_item);
+
+void push_front_album(AlbumItem **list, AlbumItem *new_item);
+
+#endif
